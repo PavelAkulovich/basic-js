18499a7ae7f0500fa21c0ba070bc5549a12087ae
PavelAkulovich <pasha860@mail.ru>
2021-10-01 13:47:38 +0300

    feat:task is finished

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,23 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+	if (options.separator === undefined) options.separator="+"
+	if (options.additionSeparator === undefined) options.additionSeparator="|"
+	if (options.addition === undefined) options.addition=""
+	if (options.repeatTimes === undefined) options.repeatTimes=1
+	if (options.additionRepeatTimes === undefined) options.additionRepeatTimes=1
+	
+  
+	let add = `${options.addition}${options.additionSeparator}`
+	let addD = `${add.repeat(options.additionRepeatTimes-1)}${options.addition}`
+
+	let st = `${str}${addD}${options.separator}`
+	let stT = `${st.repeat(options.repeatTimes-1)}${str}${addD}`
+	 
+	
+
+	
+ return stT
 }
+
